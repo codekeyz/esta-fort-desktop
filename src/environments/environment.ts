2eac78e280c7a51ea85782df5de0b17b41cdc116
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-20 20:55:31 +0000

    Added Firebase Config to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const AppConfig = {
   production: false,
-  environment: 'LOCAL'
+  environment: 'LOCAL',
+  firebase: {
+    apiKey: 'AIzaSyD2UgOq0LiKxKqKctCRGsdfxr0qIqbLPA8',
+    authDomain: 'esta-fort.firebaseapp.com',
+    databaseURL: 'https://esta-fort.firebaseio.com',
+    projectId: 'esta-fort',
+    storageBucket: 'esta-fort.appspot.com',
+    messagingSenderId: '560419373519'
+  }
 };
