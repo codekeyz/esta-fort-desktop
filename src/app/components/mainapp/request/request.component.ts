8e00ec8acfd673d234c23a7a5cd97734c64e146e
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-21 10:59:27 +0000

    Fixed layout for RequestComponent

--- src/app/components/mainapp/request/request.component.ts
+++ src/app/components/mainapp/request/request.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-request',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request.component.scss']
 })
 export class RequestComponent implements OnInit {
+  @Input() showHeader = true;
 
-  constructor() { }
+  @Input() subtitle = 'Showing All Requests';
 
-  ngOnInit() {
-  }
+  @Input() title = 'Requests';
 
+  constructor() {}
+
+  ngOnInit() {}
 }
