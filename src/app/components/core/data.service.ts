66e82922576d7c601d3c96ca8e827dd3ea866897
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-23 10:22:42 +0000

    Added Functions to DataService

--- src/app/components/core/data.service.ts
+++ src/app/components/core/data.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, QueryFn } from '@angular/fire/firestore';
 
 @Injectable()
 export class DataService {
+  constructor(private af: AngularFirestore) {}
 
-  constructor() { }
+  _getDataList<T>(collection: string, queryFn?: QueryFn) {
+    return this.af.collection<T>(collection, queryFn).valueChanges();
+  }
+
+  _getData<T>(id: string, collection: string) {
+    return this.af.doc<T>(`${collection}/${id}`).valueChanges();
+  }
 }
