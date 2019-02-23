9e1e92cf1747e2735f48c690e328b0edf03679b2
Hover Software Solutions <ositachima.co@gmail.com>
2019-02-23 10:23:31 +0000

    Refactored Dashboard Component

--- src/app/components/mainapp/dashboard/dashboard.component.ts
+++ src/app/components/mainapp/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../core/data.service';
 import { Observable } from 'rxjs';
 import { Request, User, Driver } from '../../../app.models';
+import { map, filter, tap } from 'rxjs/operators';
 declare let jQuery: any;
 
 @Component({
@@ -12,12 +13,18 @@ declare let jQuery: any;
 export class DashboardComponent implements OnInit {
   requests$: Observable<Request[]>;
   users$: Observable<User[]>;
-  drivers$: Observable<Driver[]>
+  drivers$: Observable<Driver[]>;
 
   constructor(private dataSvc: DataService) {}
 
   ngOnInit() {
-    this.requests$ = this.dataSvc._getDataList<Request>('Requests');
+    this.requests$ = this.dataSvc._getDataList<Request>('Requests').pipe(
+      tap(res =>
+        res.sort((a, b) => {
+          return a.status < b.status ? -1 : 1;
+        })
+      )
+    );
     this.users$ = this.dataSvc._getDataList<User>('Users');
     this.drivers$ = this.dataSvc._getDataList<Driver>('Drivers');
   }
