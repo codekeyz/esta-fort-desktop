9042344dc541813fa282a82acf49be9b4b47a616
Hover Software Solutions <ositachima.co@gmail.com>
2019-03-02 01:10:26 +0000

    Refactored Data models

--- src/app/app.models.ts
+++ src/app/app.models.ts
@@ -2,7 +2,12 @@ export interface Request {
   id: string;
   userName: string;
   userImage: string;
+  userID: string;
   driverName: string;
+  driverTelephone: string;
+  driverID: string;
+  is_cancelled: string;
+  is_completed: string;
   date_created: Date;
   date_confirmed: Date;
   status: number;
@@ -11,6 +16,7 @@ export interface Request {
 export interface User {
   displayImage: string;
   username: string;
+  location: string;
   email: string;
   phone: string;
   token: string;
